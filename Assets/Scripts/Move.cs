8a3f66ca63984a679c2ad212ae6c95dee3ab664f
Ceoofe <146845430+Ceoofe@users.noreply.github.com>
2025-12-01 13:58:21 -0500

    MoreOptimize

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    float speed = 10;
+    float speed = 26;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         if (transform.position.z > 88)
         {
             transform.Translate(Vector3.back * speed * Time.deltaTime);
+            speed -= 1.2f * Time.deltaTime;
         }
     }
 }
